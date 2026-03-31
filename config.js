2c7a30201cd9f2bccc6587ec921c07879bdc9bff
ChiaraDCAC <cgiralt@decampoacampo.com>
2026-03-31 15:26:59 -0300

    fix: actualizar URL GAS en config.js

--- config.js
+++ config.js
@@ -3,4 +3,6 @@
  * Incluir este archivo en index.html ANTES que app.js
  * Reemplaza la variable DCAC_WEBAPP_URL que en GAS se inyectaba via template
  */
-window.DCAC_WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbyXJVk1Zi1ucu_LXU8mXIShzBpIbikKERtkVYUUVr8UkIrxoonce0XRU15FHcEwkaqjAQ/exec';
+if (typeof window !== 'undefined') {
+  window.DCAC_WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbzgMdpchl0knP0m0GFWGUvDrcLDOVxg1rXikQnJSJvNPmRvhD0oP2Pwrjdy0eRB8jZyrA/exec';
+}
